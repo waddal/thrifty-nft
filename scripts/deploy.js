8881aa85ba0969fd9d9d8076167aab46d5802ace
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-11-06 10:40:35 -0800

    nft minting function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,18 @@
 async function main() {
-  const nftContractFactory = await hre.ethers.getContractFactory("EpicNft");
+  const nftContractFactory = await hre.ethers.getContractFactory("UglartNFT");
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
+
+  // Call the function.
+  let txn = await nftContract.makeUglartNFT();
+  // Wait for it to be mined.
+  await txn.wait();
+
+  // Mint another NFT for fun.
+  txn = await nftContract.makeUglartNFT();
+  // Wait for it to be mined.
+  await txn.wait();
 }
 
 main()
