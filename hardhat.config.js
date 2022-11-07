c32cd8491107e6bf8383a671c56b76609fc25bda
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-11-06 21:18:24 -0800

    base64 in a base64

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const ALCHEMY_API_KEY =
+  process.env.ALCHEMY_API_KEY || "https://eth-goerli/example";
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "0xkey";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
 };
